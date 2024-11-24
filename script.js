aef8d4facb31b97d9f99e3c1626fbbc7995d9a56
jayjayheyhey <villahermosajohanna96@gmail.com>
2024-11-25 02:08:01 +0800

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const signUpButton=document.getElementById('signUpButton');
-const signInButton=document.getElementById('signInButton');
-const addItemButton=document.getElementById('addItemButton');
-const dashboard=document.getElementById('dashboard');
-const addInterface = document.getElementById('addInterface');
-const signInForm=document.getElementById('signIn');
 const signUpForm=document.getElementById('signup');
+const signInButton=document.getElementById('signInButton');
+const signInForm=document.getElementById('signIn');
+const dashboard=document.getElementById('dashboard');
+const addItemButton=document.getElementById('addItemButton');
+const addInterface = document.getElementById('addInterface');
+const enterFilterCode=document.getElementById('enterFilterCode');
+const submitFilterCodeButton=document.getElementById('submitFilterCodeButton');
+const updateQuantity=document.getElementById('updateQuantity');
 
 
 signUpButton.addEventListener('click',function(){
     signInForm.style.display="none";
-    signUpForm.style.display="block";
+    signUpForm.style.display="block";   
 })
 signInButton.addEventListener('click', function(){
     signInForm.style.display="block";
@@ -27,9 +30,15 @@ if (addInterface) {
 
 if (addItemButton) {
     addItemButton.addEventListener('click', function () {     
-        // Show the addInterface
         if (addInterface) {
             addInterface.style.display = "block";
         }
+    });
+}
+
+if (submitFilterCodeButton) {
+    submitFilterCodeButton.addEventListener('click', function () {
+        enterFilterCode.style.display = "none";
+        updateQuantity.style.display = "block";
     });
 }
