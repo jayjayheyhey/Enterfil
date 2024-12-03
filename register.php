42b6e7b2b101f1093fc5768c23c153e51a2aaae6
jayjayheyhey <villahermosajohanna96@gmail.com>
2024-12-03 11:07:24 +0800

    login.php

--- register.php
+++ register.php
@@ -2,36 +2,9 @@
 
 include 'connect.php';
 
-if(isset($_POST['signUp'])){
-    $firstName=$_POST['fName'];
-    $lastName=$_POST['lName'];
-    $email=$_POST['email'];
-    $password=$_POST['password'];
-    $password=md5($password);
-
-     $checkEmail="SELECT * From users where email='$email'";
-     $result=$conn->query($checkEmail);
-     if($result->num_rows>0){
-        echo "Email Address Already Exists !";
-     }
-     else{
-        $insertQuery="INSERT INTO users(firstName,lastName,email,password)
-                       VALUES ('$firstName','$lastName','$email','$password')";
-            if($conn->query($insertQuery)==TRUE){
-                header("location: index.php");
-            }
-            else{
-                echo "Error:".$conn->error;
-            }
-     }
-   
-
-}
-
 if(isset($_POST['signIn'])){
    $email=$_POST['email'];
    $password=$_POST['password'];
-   $password=md5($password) ;
    
    $sql="SELECT * FROM users WHERE email='$email' and password='$password'";
    $result=$conn->query($sql);
@@ -43,7 +16,9 @@ if(isset($_POST['signIn'])){
     exit();
    }
    else{
-    echo "Not Found, Incorrect Email or Password";
+      header("Location: index.php?error=1");
+      exit();
+    exit();
    }
 
 }
