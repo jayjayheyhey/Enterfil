0908c77ba35176fb678edeb817904445ce5f031f
jayjayheyhey <villahermosajohanna96@gmail.com>
2024-12-04 13:00:52 +0800

    update quantity

--- updateFilter.php
+++ updateFilter.php
@@ -9,23 +9,34 @@ if(isset($_POST['updateButton'])){
     $MaxStock = $_POST['maxStock'];
     $LowStockSignal = $_POST['lowStock'];
 
-
-    $updateQuery = "UPDATE filters 
-                    SET FilterName = '$FilterName', 
-                        Materials = '$Materials', 
-                        Quantity = '$Quantity', 
-                        MaxStock = '$MaxStock', 
-                        LowStockSignal = '$LowStockSignal'
-                    WHERE FilterCode = '$FilterCode'";
-
-    if($conn->query($updateQuery) === TRUE){
+    if ($Quantity > $MaxStock) {
         echo '<script>
-            alert("Filter successfully updated");
-            window.location.href = "homepage.php";
-          </script>';
-        exit();
-    } else {
-        echo "Error: " . $conn->error; 
+                alert("ERROR: Quantity can not be larger than the maximum stock.");
+                window.location.href = "addInterface.php";
+            </script>';
+    }elseif ($Quantity < 0) {
+        echo '<script>
+                alert("ERROR: Quantity can not be lower than 0.");
+                window.location.href = "addInterface.php";
+            </script>';
+    }else {
+        $updateQuery = "UPDATE filters 
+                        SET FilterName = '$FilterName', 
+                            Materials = '$Materials', 
+                            Quantity = '$Quantity', 
+                            MaxStock = '$MaxStock', 
+                            LowStockSignal = '$LowStockSignal'
+                        WHERE FilterCode = '$FilterCode'";
+
+        if($conn->query($updateQuery) === TRUE){
+            echo '<script>
+                alert("Filter successfully updated");
+                window.location.href = "homepage.php";
+            </script>';
+            exit();
+        } else {
+            echo "Error: " . $conn->error; 
+        }
     }
 }
 ?>
