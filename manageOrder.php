60211953a7ec996383994fdccfd663ad691e6007
jins-jinx <itsallaboutjins@gmail.com>
2025-05-09 01:42:39 +0800

    a

--- manageOrder.php
+++ manageOrder.php
@@ -120,7 +120,7 @@ if (!$singleDeletion) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manage Orders</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link rel="stylesheet" href="orderFormDashboard4.css">
+    <link rel="stylesheet" href="orderFormDashboard5.css">
     <link rel="stylesheet" href="manageOrder1.css">
 
 </head>
--- salesReport.php
+++ salesReport.php
@@ -104,7 +104,7 @@ function formatCurrency($amount) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sales Report</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link rel="stylesheet" href="orderFormDashboard4.css">
+    <link rel="stylesheet" href="orderFormDashboard5.css">
     <link rel="stylesheet" href="salesReport.css">
 </head>
 <body>
